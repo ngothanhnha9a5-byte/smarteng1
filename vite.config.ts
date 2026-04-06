302019681ff8d4d9a7943a3f111f72430301af7b
ngothanhnha9a5-byte <ngothanhnha9a5@gmail.com>
2026-04-06 20:51:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/smartengdemo/' // ⚠️ QUAN TRỌNG
+  base: '/smartengdemo/' // 👈 tên repo của bạn
 })
